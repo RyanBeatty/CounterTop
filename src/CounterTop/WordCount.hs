d926d3258ff5911fe229a76b2dc3c5dad2d84873
RyanBeatty <rvbeatty@email.wm.edu>
2016-08-28 18:59:10 -0700

    finished writing WordCount module

--- src/CounterTop/WordCount.hs
+++ src/CounterTop/WordCount.hs
@@ -2,19 +2,28 @@ module CounterTop.WordCount() where
 
 import qualified Data.Map.Strict as Map
 
+-- Type Synonym for a map from Strings to Integers
 type WordCount = Map.Map String Integer
 
+-- Simple Constructor for WordCount
+-- Creates an empty map
 wordCount :: WordCount
 wordCount = Map.empty
 
+-- Adds a word to the word count map if it doesn't exist
+-- and increments occurence if it does
 processWord :: String -> WordCount -> WordCount
 processWord word wc = Map.insertWith (+) word 1 wc
 
+-- Counts all word occurences in a line
+proccessLine :: String -> WordCount -> WordCount
+proccessLine line wc = foldr (processWord) wc . words $ line
 
---countWords :: String -> WordCount
---countWords contents = foldr processWord empty .  words . lines 
+-- Counts all words in a text chunk
+proccessContents :: String -> WordCount
+proccessContents = foldr (proccessLine) wordCount . lines
 
-
-
-
+-- simple name binding for proccessContents
+countWords :: String -> WordCount
+countWords = proccessContents
 
